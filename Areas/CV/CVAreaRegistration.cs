bc714702062a4af22379bc49dd7121d8d959821a
Abhishek Kapoor <abhishekkpr9@gmail.com>
2018-01-15 15:07:55 +0530

    adding master pages

---
+++ Areas/CV/CVAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SoftwareCorners.Areas.CV
+{
+    public class CVAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "CV";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "CV_default",
+                "CV/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Areas/CV/Controllers/CVController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoftwareCorners.Areas.CV.Controllers
+{
+    public class CVController : Controller
+    {
+        // GET: CV/CV
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
